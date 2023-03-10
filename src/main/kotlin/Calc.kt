1de046cd6718263bd7c1620c241fd0a758975070
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2023-03-10 15:24:17 +0300

    simple calculator

---
+++ src/main/kotlin/Calc.kt
@@ -0,0 +1,11 @@
+fun add(first :Int, second :Int) = first + second
+fun subtraction(first :Int, second :Int) = first - second
+fun multiplication(first :Int, second :Int) = first * second
+fun division(first :Int, second :Int) = first / second
+
+fun main() {
+    println(add(1,2))
+    println(subtraction(2, 1))
+    println(division(2,1))
+    println(multiplication(1,2))
+}
