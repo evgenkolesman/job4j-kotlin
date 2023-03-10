f134a7ac450467e60578e4369a0518c07446cb53
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2023-03-10 17:27:23 +0300

    Max from 3 with old max (updated)

--- src/main/kotlin/ru/kolesnikov/MaxRealization.kt
+++ src/main/kotlin/ru/kolesnikov/MaxRealization.kt
@@ -2,7 +2,9 @@ package ru.kolesnikov
 
 fun max(first: Int, second: Int) = if (first > second) first else second
 
-fun max3(first: Int, second: Int, third: Int) = if( third > max(first, second)) third else max(first, second)
+fun max3(first: Int, second: Int, third: Int) =  max(max(first, second), third)
+
+
 fun main() {
     val rsl = max3(1, 2, 5)
     println("max is $rsl")
