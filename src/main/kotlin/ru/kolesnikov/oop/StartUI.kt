0856d09847917d2b4f1bb825fe3ad87d3bc2be5e
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2023-04-08 13:38:03 +0300

    StartUI with companion object

--- src/main/kotlin/ru/kolesnikov/oop/StartUI.kt
+++ src/main/kotlin/ru/kolesnikov/oop/StartUI.kt
@@ -2,17 +2,34 @@ package ru.kolesnikov.oop
 
 import java.util.*
 
-object StartUI {
+class StartUI private constructor() {
+    companion object {
+        internal val cache = ArrayList<String>()
+        internal fun addString(text: String) = this.cache.add(String.format(" \"%s\" ", text.replace("/add ", "")))
+        internal fun getAll() = println(cache)
+        internal fun getElement(text: String) {
+            val index = text.replace("/element", "")
 
-    private val cache = ArrayList<String>()
+            if (text.replace("/element", "")
+                    .trim() == "") println("введите индекс")
+            else if (index
+                    .trim()
+                    .toInt() > cache.size
+            ) println("Индекс введен не корректно введите его в рамках длины " + cache.size)
+            else println(cache[index.trim().toInt() - 1])
+        }
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        var exit = 0
+        internal fun exit(): Int = 1
 
-        while (exit != 1) {
-            println(
-                """
+    }
+}
+
+fun main(args: Array<String>) {
+    var exit = 0
+
+    while (exit != 1) {
+        println(
+            """
             Здравствуйте введите команду:
             - /add чтобы добавить запись, на этой же строчке введите запись
             - /get чтобы вывести список записей
@@ -21,29 +38,13 @@ object StartUI {
             /element index(числовое значение)
             - /end чтобы закончить
         """.trimIndent()
-            )
-            val scanner = Scanner(System.`in`)
-            val nextLine = scanner.nextLine()
-            if (nextLine.startsWith("/add")) addString(nextLine)
-            if (nextLine.startsWith("/get")) getAll()
-            if (nextLine.startsWith("/element")) getElement(nextLine)
-            if (nextLine.startsWith("/end")) exit = exit()
-        }
-
+        )
+        val scanner = Scanner(System.`in`)
+        val nextLine = scanner.nextLine()
+        if (nextLine.startsWith("/add")) StartUI.Companion.addString(nextLine)
+        if (nextLine.startsWith("/get")) StartUI.Companion.getAll()
+        if (nextLine.startsWith("/element")) StartUI.Companion.getElement(nextLine)
+        if (nextLine.startsWith("/end")) exit = StartUI.Companion.exit()
     }
 
-   private fun addString(text : String) = this.cache.add(String.format(" \"%s\" " , text.replace("/add ", "")))
-   private fun getAll() = println(cache)
-   private fun getElement(text: String)  {
-       val index = text.replace("/element", "")
-
-       if(text
-           .replace("/element", "")
-           .trim() == "") println("введите индекс")
-       else if(index
-           .trim()
-           .toInt() > cache.size) println("Индекс введен не корректно введите его в рамках длины " + cache.size)
-       else println(cache[index.trim().toInt() - 1])
-   }
-    private fun exit(): Int = 1
 }
